6b7516184293dae9a045295122e7a91e4b9bdf21
Nastassia Danilova <nastassia.danilova@epicmax.co>
2019-09-24 13:52:07 +0000

    add users thunks, 'api/v1/relationships' request, update .gitignore

--- src/api/users.js
+++ src/api/users.js
@@ -1,16 +1,24 @@
 import utils from './utils.js'
 
+const baseUrl = '/api/v1/accounts'
 const Users = {
   async get ({ config, params: { id } }) {
     return utils.request({
       config,
-      url: `/api/v1/accounts/${id}`
+      url: `${baseUrl}/${id}`
     })
   },
   async statuses ({ config, params, queries }) {
     return utils.request({
       config,
-      url: `/api/v1/accounts/${params.id}/statuses`,
+      url: `${baseUrl}/${params.id}/statuses`,
+      queries
+    })
+  },
+  async relationships ({ config, queries }) {
+    return utils.request({
+      config,
+      url: `${baseUrl}/relationships`,
       queries
     })
   },
