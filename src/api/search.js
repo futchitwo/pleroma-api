b420c5bb832e9baede390e94071936a4f1af7326
Shpuld Shpuldson <shp@cock.li>
2020-02-26 13:04:00 +0200

    fix lint

--- src/api/search.js
+++ src/api/search.js
@@ -2,7 +2,7 @@ import utils from './utils'
 
 const SEARCH_URL = '/api/v2/search'
 
-const Search = async function({ config, queries }) {
+const Search = async function ({ config, queries }) {
   return utils.request({
     config,
     url: SEARCH_URL,
