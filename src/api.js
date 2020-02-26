068f03d9836fc105ab4e5f81343740f40276c59e
Nastassia Danilova <nastassia.danilova@epicmax.co>
2020-02-26 10:59:41 +0000

    Search

--- src/api.js
+++ src/api.js
@@ -5,6 +5,7 @@ import notifications from './api/notifications'
 import oauth from './api/oauth'
 import media from './api/media'
 import conversations from './api/conversations'
+import search from './api/search'
 import polls from './api/polls'
 
 export default {
@@ -15,5 +16,6 @@ export default {
   oauth,
   media,
   conversations,
+  search,
   polls
 }
